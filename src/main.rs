8596f3094203ca457407a590237c3f60d059cb3c
Gökmen Kaplan <48455693+x-gkm@users.noreply.github.com>
2025-12-11 03:08:21 +0300

    Add feed endpoints

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::{env, sync::Arc};
 
 use anyhow::anyhow;
 use axum::{
-    Form, RequestPartsExt, Router,
-    extract::{FromRequestParts, State},
+    Form, Json, RequestPartsExt, Router,
+    extract::{FromRequestParts, Path, State},
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::{get, post},
 };
 use axum_extra::extract::CookieJar;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 
 use crate::auth::{Auth, AuthError};
@@ -98,6 +98,84 @@ impl FromRequestParts<AppState> for User {
     }
 }
 
+#[derive(Serialize, Deserialize)]
+struct Feed {
+    url: String,
+    name: String,
+    selectors: Selectors,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Selectors {
+    post: Option<String>,
+    title: Option<String>,
+    link: String,
+    description: Option<String>,
+    date: Option<String>,
+    image: Option<String>,
+}
+
+impl From<db::WebNews> for Feed {
+    fn from(web_news: db::WebNews) -> Feed {
+        Feed {
+            url: web_news.url,
+            name: web_news.name,
+            selectors: Selectors {
+                post: web_news.selector_post,
+                title: web_news.selector_title,
+                link: web_news.selector_link,
+                description: web_news.selector_description,
+                date: web_news.selector_date,
+                image: web_news.selector_image,
+            },
+        }
+    }
+}
+
+async fn create_feed(
+    State(state): State<AppState>,
+    user: User,
+    Json(feed): Json<Feed>,
+) -> Result<(), AppError> {
+    state
+        .db
+        .create_web_news(&db::WebNews {
+            id: -1, // ignored
+            url: feed.url,
+            name: feed.name,
+            owner: user.id,
+            selector_post: feed.selectors.post,
+            selector_title: feed.selectors.title,
+            selector_link: feed.selectors.link,
+            selector_description: feed.selectors.description,
+            selector_date: feed.selectors.date,
+            selector_image: feed.selectors.image,
+        })
+        .await?;
+
+    Ok(())
+}
+
+async fn list_feeds(
+    State(state): State<AppState>,
+    user: User,
+) -> Result<Json<Vec<String>>, AppError> {
+    Ok(Json(
+        state.db.get_all_web_news_names_by_user_id(user.id).await?,
+    ))
+}
+
+async fn get_feed(
+    State(state): State<AppState>,
+    Path(feed): Path<String>,
+) -> Result<Json<Feed>, AppError> {
+    let Some(web_news) = state.db.get_web_news_by_name(&feed).await? else {
+        return Err(anyhow!("no such feed").into());
+    };
+
+    Ok(Json(web_news.into()))
+}
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     _ = dotenvy::dotenv();
@@ -111,6 +189,8 @@ async fn main() -> Result<(), anyhow::Error> {
                 .route("/register", post(register_user))
                 .route("/login", post(login_user)),
         )
+        .route("/feeds", get(list_feeds).post(create_feed))
+        .route("/feeds/{name}", get(get_feed))
         .with_state(AppState {
             auth: Auth::new(Arc::clone(&db)),
             db: Arc::clone(&db),
